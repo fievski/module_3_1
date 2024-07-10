996876bbef01a0fed3321e8b728d08852321ef5d
fievski <fievski@yandex.ru>
2024-07-11 00:15:26 +0600

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+     count_calls()
+     return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    new_list = []
+    for item in list_to_search:
+        new_list.append(item.lower())
+    if string.lower() in new_list:
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
